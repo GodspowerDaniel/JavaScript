589bd0cd606dbfef0d8b8e1e629dad17454b0fee
DanTower <godspowerdaniel72@gmail.com>
2023-07-19 09:13:29 +0100

    commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let date = new Date();
+let staticDate = new Date(2005, 1, 11);
+document.getElementById('new_date').textContent = date;
+document.getElementById('new_date').style.fontWeight = 'bold'
+
+document.getElementById('new_datee').textContent = staticDate;
+document.getElementById('new_datee').style.fontWeight = 'bold'
