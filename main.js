f7dea8a4149de9c5cba4f1076c606a7f248e0819
DanTower <godspowerdaniel72@gmail.com>
2023-07-20 21:21:02 +0100

    commit

--- main.js
+++ main.js
@@ -15,17 +15,17 @@ document.getElementById('new_date').style.fontWeight = 'bold'
 document.getElementById('new_datee').textContent = staticDate;
 document.getElementById('new_datee').style.fontWeight = 'bold'
 
-clickToCheck.addEventListener('click', function(){
+clickToCheck.addEventListener('click', function () {
     // alert('its correct');
     const enterAmount = parseInt(document.getElementById('money').value);
 
-if (enterAmount >= 5000){
-    result.textContent = 'You can get just a bottle, Proceed to Checkout session Now!';
-} else {
-    result.innerHTML = 'You do not have enough fund, make it 5k and come back later';
-}
+    if (enterAmount >= 5000) {
+        result.textContent = 'You can get just a bottle, Proceed to Checkout session Now!';
+    } else {
+        result.innerHTML = 'You do not have enough fund, make it 5k and come back later';
+    }
 });
 
-generateOtp.addEventListener('click', function(){
+generateOtp.addEventListener('click', function () {
     otpDisplay.textContent = Math.floor(Math.random() * 1000000);
 });
